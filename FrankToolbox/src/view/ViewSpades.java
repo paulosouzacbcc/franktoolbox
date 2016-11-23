4aa9a34e34330a45015e5c0e5f3a2f1ef0d62535
paulosouza <paulosouza@maverick>
2016-11-23 00:00:16 -0300

    inicio tela SPAdes

--- FrankToolbox/src/view/ViewSpades.java
+++ FrankToolbox/src/view/ViewSpades.java
@@ -31,12 +31,14 @@ public class ViewSpades extends javax.swing.JInternalFrame {
     private void initComponents() {
 
         jTabbedPane2 = new javax.swing.JTabbedPane();
-        jScrollPane2 = new javax.swing.JScrollPane();
+        jScrollPaneInput = new javax.swing.JScrollPane();
+        jPanel4 = new javax.swing.JPanel();
+        jScrollPaneBasic = new javax.swing.JScrollPane();
         jPanel1 = new javax.swing.JPanel();
         jLabel1 = new javax.swing.JLabel();
         jTextField1 = new javax.swing.JTextField();
         jButton1 = new javax.swing.JButton();
-        jScrollPane1 = new javax.swing.JScrollPane();
+        jScrollPaneAdvanced = new javax.swing.JScrollPane();
         jPanel2 = new javax.swing.JPanel();
         jLabel2 = new javax.swing.JLabel();
         jTextField2 = new javax.swing.JTextField();
@@ -52,14 +54,29 @@ public class ViewSpades extends javax.swing.JInternalFrame {
         jLabel7 = new javax.swing.JLabel();
         jTextFieldKmer = new javax.swing.JTextField();
         jButton4 = new javax.swing.JButton();
-        jScrollPane3 = new javax.swing.JScrollPane();
+        jScrollPanePipeline = new javax.swing.JScrollPane();
         jPanel3 = new javax.swing.JPanel();
         jCheckBox1 = new javax.swing.JCheckBox();
         jCheckBox2 = new javax.swing.JCheckBox();
         jCheckBox3 = new javax.swing.JCheckBox();
         jCheckBox4 = new javax.swing.JCheckBox();
         jCheckBox5 = new javax.swing.JCheckBox();
-        jButton3 = new javax.swing.JButton();
+        jButtonProcessar = new javax.swing.JButton();
+
+        javax.swing.GroupLayout jPanel4Layout = new javax.swing.GroupLayout(jPanel4);
+        jPanel4.setLayout(jPanel4Layout);
+        jPanel4Layout.setHorizontalGroup(
+            jPanel4Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 748, Short.MAX_VALUE)
+        );
+        jPanel4Layout.setVerticalGroup(
+            jPanel4Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 388, Short.MAX_VALUE)
+        );
+
+        jScrollPaneInput.setViewportView(jPanel4);
+
+        jTabbedPane2.addTab("Input", jScrollPaneInput);
 
         jPanel1.setBorder(javax.swing.BorderFactory.createTitledBorder(null, "SPAdes", javax.swing.border.TitledBorder.CENTER, javax.swing.border.TitledBorder.DEFAULT_POSITION));
 
@@ -91,9 +108,9 @@ public class ViewSpades extends javax.swing.JInternalFrame {
                 .addContainerGap(395, Short.MAX_VALUE))
         );
 
-        jScrollPane2.setViewportView(jPanel1);
+        jScrollPaneBasic.setViewportView(jPanel1);
 
-        jTabbedPane2.addTab("Basic Options", jScrollPane2);
+        jTabbedPane2.addTab("Basic Options", jScrollPaneBasic);
 
         jPanel2.setBorder(javax.swing.BorderFactory.createTitledBorder(null, "SPAdes", javax.swing.border.TitledBorder.CENTER, javax.swing.border.TitledBorder.DEFAULT_POSITION));
 
@@ -174,16 +191,17 @@ public class ViewSpades extends javax.swing.JInternalFrame {
                     .addComponent(jLabel6)
                     .addComponent(jTextField5, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
                 .addGap(18, 18, 18)
-                .addGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(jLabel7)
-                    .addComponent(jTextFieldKmer, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addComponent(jButton4))
+                .addGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(jButton4, javax.swing.GroupLayout.Alignment.TRAILING)
+                    .addGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                        .addComponent(jLabel7)
+                        .addComponent(jTextFieldKmer, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)))
                 .addContainerGap(100, Short.MAX_VALUE))
         );
 
-        jScrollPane1.setViewportView(jPanel2);
+        jScrollPaneAdvanced.setViewportView(jPanel2);
 
-        jTabbedPane2.addTab("Advanced Options", jScrollPane1);
+        jTabbedPane2.addTab("Advanced Options", jScrollPaneAdvanced);
 
         jPanel3.setBorder(javax.swing.BorderFactory.createTitledBorder(null, "SPAdes", javax.swing.border.TitledBorder.CENTER, javax.swing.border.TitledBorder.DEFAULT_POSITION));
 
@@ -227,11 +245,11 @@ public class ViewSpades extends javax.swing.JInternalFrame {
                 .addContainerGap(229, Short.MAX_VALUE))
         );
 
-        jScrollPane3.setViewportView(jPanel3);
+        jScrollPanePipeline.setViewportView(jPanel3);
 
-        jTabbedPane2.addTab("Pipeline Options", jScrollPane3);
+        jTabbedPane2.addTab("Pipeline Options", jScrollPanePipeline);
 
-        jButton3.setText("Processar");
+        jButtonProcessar.setText("Processar");
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
@@ -240,7 +258,7 @@ public class ViewSpades extends javax.swing.JInternalFrame {
             .addComponent(jTabbedPane2, javax.swing.GroupLayout.DEFAULT_SIZE, 758, Short.MAX_VALUE)
             .addGroup(layout.createSequentialGroup()
                 .addGap(0, 0, Short.MAX_VALUE)
-                .addComponent(jButton3))
+                .addComponent(jButtonProcessar))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
@@ -248,7 +266,7 @@ public class ViewSpades extends javax.swing.JInternalFrame {
                 .addContainerGap()
                 .addComponent(jTabbedPane2, javax.swing.GroupLayout.DEFAULT_SIZE, 426, Short.MAX_VALUE)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                .addComponent(jButton3))
+                .addComponent(jButtonProcessar))
         );
 
         pack();
@@ -263,8 +281,8 @@ public class ViewSpades extends javax.swing.JInternalFrame {
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JButton jButton1;
     private javax.swing.JButton jButton2;
-    private javax.swing.JButton jButton3;
     private javax.swing.JButton jButton4;
+    private javax.swing.JButton jButtonProcessar;
     private javax.swing.JCheckBox jCheckBox1;
     private javax.swing.JCheckBox jCheckBox2;
     private javax.swing.JCheckBox jCheckBox3;
@@ -281,9 +299,11 @@ public class ViewSpades extends javax.swing.JInternalFrame {
     private javax.swing.JPanel jPanel1;
     private javax.swing.JPanel jPanel2;
     private javax.swing.JPanel jPanel3;
-    private javax.swing.JScrollPane jScrollPane1;
-    private javax.swing.JScrollPane jScrollPane2;
-    private javax.swing.JScrollPane jScrollPane3;
+    private javax.swing.JPanel jPanel4;
+    private javax.swing.JScrollPane jScrollPaneAdvanced;
+    private javax.swing.JScrollPane jScrollPaneBasic;
+    private javax.swing.JScrollPane jScrollPaneInput;
+    private javax.swing.JScrollPane jScrollPanePipeline;
     private javax.swing.JTabbedPane jTabbedPane2;
     private javax.swing.JTextField jTextField1;
     private javax.swing.JTextField jTextField2;
